5494bd642b7f7ae63838c7ea4ba811b6d092e4b9
Leonardo-Jodeve <dszw2000@gmail.com>
2021-03-18 14:19:22 +0800

    Normalization

--- SipmleTest/Count9.cpp
+++ SipmleTest/Count9.cpp
@@ -1,23 +1,28 @@
 //
 // Created by Leonardo Jodeve on 2021/3/18.
 //
+
 #include <stdio.h>
 
 int Count9(int n)
 {
     int count = 0;
-    for(int i=0; i<n+1; i++)
+
+    for(int i = 0; i < n+1; i++)
     {
         int temp = i;
         int checkIfNine = 0;
         while(temp != 0)
         {
-            checkIfNine = temp%10;
+            checkIfNine = temp % 10;
             if(checkIfNine == 9)
+            {
                 count++;
-            temp = temp/10;
+            }
+            temp = temp / 10;
         }
     }
+
     return count;
 }
 
